dac6122d791a5fde607568df14bb88bbf8f6d655
VARGAS Mauricio <vargasmauricio@MacBook-Pro-de-Servicedesk.local>
2025-01-13 17:18:33 -0300

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-}
+    id("com.google.gms.google-services")}
 
 android {
     namespace = "com.example.firebaseapp"
@@ -56,4 +56,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.firebase.bom)
+    implementation(libs.firebase.analytics)
 }
